db1b882c8ca6aa941cc594767a5b5aa575fa332a
AthirK <athirkazem@gmail.com>
2025-01-21 11:13:52 +0100

    Start Code

---
+++ src/main/java/com/example/AsciiAPI/AsciiApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.AsciiAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsciiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsciiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AsciiAPI/AsciiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AsciiAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsciiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

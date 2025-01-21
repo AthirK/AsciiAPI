5fb77bfbfbb9fc7366a1e6bd0399244081efd708
JoLundan <johanna.lundby@gmail.com>
2025-01-21 11:40:48 +0100

    Johanna test

--- src/main/java/com/example/AsciiAPI/AsciiApiApplication.java
+++ src/main/java/com/example/AsciiAPI/AsciiApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AsciiApiApplication {
 
+	// Johanna här!
+
 	public static void main(String[] args) {
 		SpringApplication.run(AsciiApiApplication.class, args);
 	}
